364a3af8d594b505a1db4eb4b70716a6e3688a9c
JusticeWheelwright <justice792@outlook.com>
2023-06-02 17:59:23 -0700

    comments

--- client.py
+++ client.py
@@ -1,25 +1,30 @@
 import time, socket, sys
 
+#sets up client
 socket_server = socket.socket()
 server_host = socket.gethostname()
 ip = socket.gethostbyname(server_host)
 sport = 8080
 
-print('This is your IP address: ',ip)
+#asks user for the server to connect to
+print('your IP address: ',ip)
 server_host = input('Enter friend\'s IP address:')
-name = input('Enter Friend\'s name: ')
+name = input('Enter your name: ')
 
-
+#connects to specified server
 socket_server.connect((server_host, sport))
 
+#sends and recieves server name information
 socket_server.send(name.encode())
 server_name = socket_server.recv(1024)
 server_name = server_name.decode()
 
+#asks the user if they want to continue the last conversation and returns the response to the server
 print(server_name,' has joined...')
 cont = input("continue last conversation? (y/n)")
 socket_server.send(cont.encode())
 
+#recieves content of last conversation from the server and prints until the file holding the conversation is empty
 log = True
 while log != False:
     message = (socket_server.recv(1024)).decode()
@@ -28,7 +33,7 @@ while log != False:
     else:
         print(message)
 
-
+# recieves and prints messages to the server
 while True:
     message = (socket_server.recv(1024)).decode()
     print(server_name, ":", message)
--- server.py
+++ server.py
@@ -1,32 +1,39 @@
 import time, socket, sys
- 
+
+# create server 
 new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_name = socket.gethostname()
 s_ip = socket.gethostbyname(host_name)
  
 port = 8080
  
+# bind socket and then create user name for conversations
 new_socket.bind((host_name, port))
-print("Binding successful!")
-print("This is your IP: ", s_ip)
+print("your IP: ", s_ip)
  
 name = input('Enter name: ')
  
+# listen for client
 new_socket.listen(1) 
  
- 
+# accept client
 conn, add = new_socket.accept()
  
 print("Received connection from ", add[0])
 print('Connection Established. Connected From: ',add[0])
  
+# get client name
 client = (conn.recv(1024)).decode()
-print(client + ' has connected.')
+print(client + ' connected.')
 
+# file system - gets message from the client on whether they want to continue previous conversation or not
 message = conn.recv(1024)
 message = message.decode()
+conn.send(name.encode())
 f = open("log.txt")
+# if client wants to continue conversation
 if message == "y":
+    # reads all the contents of the file and prints them out locally and encodes them to the client until it reaches the end of the file
     for line in f:
         message = f.readline()
         if message == '':
@@ -35,17 +42,18 @@ if message == "y":
             print(message)
             conn.send(message.encode())
     f = open("log.txt", "a")
+# if user does not want to continue previous conversation, overwrite the file
 else:
     f = open("log.txt", "w")
  
 
-
-conn.send(name.encode())
+# main loop - takes input, sends it to client and also prints out messages from the client. 
+# This function also prints out all messages, to file so it can be continued later
 while True:
     message = input(host_name, ':')
-    f.write(message)
+    f.write(host_name, ':', message)
     conn.send(message.encode())
     message = conn.recv(1024)
     message = message.decode()
-    f.write(message)
+    f.write(client, ':', message)
     print(client, ':', message)
